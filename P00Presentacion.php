d1a2dcbb978d4c1b43255f140a34fc244c79156b
jaimemv23 <jaimemiranda.logcd@gmail.com>
2024-10-21 09:29:29 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,14 @@
+<?php
+/* CBTIS 89 
+   Programa que muestra el dia, sueldo de una persona
+   Jaime Eduardo Miranda Vázquez
+   3°A Programación T.M.
+*/
+
+  echo"Jaime Eduardo Miranda Vázquez <br>";
+  echo"Durango, Dgo <br>";
+  echo "CBTIS 89 <br>";  
+  echo "Programación <br>";
+  echo "3°A T.M. <br>";
+
+  ?>
