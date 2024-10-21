3435a870813e39e56fdaacef26e436afed38aa07
jaimemv23 <jaimemiranda.logcd@gmail.com>
2024-10-21 09:09:42 -0600

    Add files via upload

---
+++ P01Variables1.php
@@ -0,0 +1,25 @@
+<?php
+/* CBTIS 89 
+   Programa que muestra el dia, sueldo de una persona
+   Jaime Eduardo Miranda Vázquez
+   3°A Programación T.M.
+*/
+
+$dia = 24; //Se declara una variable de tipo integer.
+$sueldo = 758.43; //Se declara una variable de tipo double.
+$nombre = "Juan"; //Se declara una variable de tipo string.
+$existe = true; //Se declara una variable boolean.
+
+echo "Variable entera:";
+echo $dia; echo "<br>";
+echo "Variable double";
+echo $sueldo; echo "<br>";
+echo "Variable string:";
+echo $nombre; echo "<br>";
+echo"Variable boolean:";
+echo $existe;
+echo "<br>";
+$cadena1= "Hola";
+$cadena2= "Mundo";
+echo $cadena1. "      ".$cadena2;
+?>
